c452e3d015e772ccc62434017197dd592f2314ac
ligi <ligi@ligi.de>
2017-10-07 19:18:23 +0200

    setTransactionSuccessful()

--- kroom/src/main/java/org/ligi/kroom/RoomFun.kt
+++ kroom/src/main/java/org/ligi/kroom/RoomFun.kt
@@ -2,8 +2,10 @@ package org.ligi.kroom
 
 import android.arch.persistence.room.RoomDatabase
 
-fun <T : RoomDatabase> T.inTransaction(call: T.() -> Unit) {
+fun <T : RoomDatabase> T.inTransaction(call: T.() -> Unit) = try {
     beginTransaction()
     call.invoke(this)
+    setTransactionSuccessful()
+} finally {
     endTransaction()
 }
